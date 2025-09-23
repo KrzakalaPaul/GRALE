f7d4deb647c3ed8c81c51e404645e16d1c17521a
PaulKrzakala <paul.krzakala@gmail.com>
2025-09-23 19:24:42 +0200

    update cpu per task

--- scripts/jeanzay_main.sh
+++ scripts/jeanzay_main.sh
@@ -6,7 +6,7 @@
 #SBATCH --nodes=1                    # on demande un noeud
 #SBATCH --ntasks-per-node=4          # avec une tache par noeud (= nombre de GPU ici)
 #SBATCH --gres=gpu:4                 # nombre de GPU par noeud (max 8 avec gpu_p2, gpu_p5)
-#SBATCH --cpus-per-task=96           # nombre de CPU par tache pour gpu_p5 (1/8 des CPU du noeud 8-GPU A100)
+#SBATCH --cpus-per-task=24           # nombre de CPU par tache pour gpu_p5 (1/8 des CPU du noeud 8-GPU A100)
 # /!\ Attention, "multithread" fait reference à l'hyperthreading dans la terminologie Slurm
 #SBATCH --hint=nomultithread         # hyperthreading desactive
 #SBATCH --time=100:00:00              # temps maximum d'execution demande (HH:MM:SS)
